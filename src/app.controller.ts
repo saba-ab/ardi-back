0cdaefbe90e02786470c74c20f328a10ccb28f58
saba-ab <sabaabjandadze@gmail.com>
2024-04-23 15:51:36 +0400

    feat (host) implementing vercel config

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,4 +4,8 @@ import { AppService } from './app.service';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+  @Get()
+  root() {
+    return this.appService.getHello();
+  }
 }
